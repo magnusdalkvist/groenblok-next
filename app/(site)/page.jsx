9e6ebc4fb8906bb9c66e344522b5f10e5a12286d
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-15 18:28:16 +0200

    use client test

---
+++ app/(site)/page.jsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { getHome } from "../../sanity/sanity-utils";
+import RenderModules from "./components/RenderModules";
+
+export default async function Home() {
+  const home = await getHome();
+
+  return <RenderModules modules={home.modules} />;
+}
