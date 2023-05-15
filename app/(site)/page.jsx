c7c1f7b48805989c15737e8bb252c64ea2f2d69c
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-15 18:20:16 +0200

    removed page

--- app/(site)/page.jsx
+++
@@ -1,8 +0,0 @@
-import { getHome } from "../../sanity/sanity-utils";
-import RenderModules from "./components/RenderModules";
-
-export default async function Home() {
-  const home = await getHome();
-
-  return <RenderModules modules={home.modules} />;
-}
