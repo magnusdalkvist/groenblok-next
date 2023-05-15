dc813830a2b2dde1229c30da6db3da382ac3cc53
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-15 20:41:44 +0200

    Added SSP

--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,8 +1,14 @@
 import { getHome } from "../../sanity/sanity-utils";
 import RenderModules from "./components/RenderModules";
 
-export default async function Home() {
+export default async function Home({ home }) {
+  return <RenderModules modules={home.modules} />;
+}
+
+export async function getServerSideProps(context) {
   const home = await getHome();
 
-  return <RenderModules modules={home.modules} />;
+  return {
+    props: { home }, // will be passed to the page component as props
+  };
 }
