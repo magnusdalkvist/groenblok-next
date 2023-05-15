c7db6baef58e772f74a7bb00b3864cf1bf500f51
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-15 18:39:20 +0200

    idk what to dooooo

--- app/(site)/components/RenderModules.jsx
+++ app/(site)/components/RenderModules.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Hero from "./modules/Hero";
 import ShopSection from "./modules/ShopSection";
 import SlidingInfoCards from "./modules/SlidingInfoCards";
--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,10 +1,8 @@
-"use client";
-
 import { getHome } from "../../sanity/sanity-utils";
 import RenderModules from "./components/RenderModules";
 
 export default async function Home() {
   const home = await getHome();
-  console.log(home);
+
   return <RenderModules modules={home.modules} />;
 }
