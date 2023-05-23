2faf2f5c5dfe480af12d43ad2fd3e8fa9b9064f1
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-23 10:52:56 +0200

    Added accordion and updated articles

---
+++ app/(site)/components/Accordion.jsx
@@ -0,0 +1,39 @@
+import clsx from "clsx";
+import { useState } from "react";
+
+export default function Accordion({ title, children }) {
+  const [isShowing, setIsShowing] = useState(false);
+
+  const toggle = () => {
+    setIsShowing(!isShowing);
+  };
+
+  return (
+    <div>
+      <button
+        className="flex justify-between items-center w-full text-[30px] relative cursor-pointer"
+        onClick={toggle}
+        type="button"
+      >
+        <p>{title}</p>
+        <svg
+          className={clsx(
+            "w-8 h-4 transition-all duration-150",
+            isShowing && "transform rotate-180"
+          )}
+          width="40"
+          height="23"
+          viewBox="0 0 40 23"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M17.9828 22.0203C19.0985 23.1361 20.9104 23.1361 22.0261 22.0203L39.1632 4.88074C40.2789 3.76489 40.2789 1.95274 39.1632 0.83689C38.0475 -0.278963 36.2356 -0.278963 35.1199 0.83689L20 15.9589L4.88006 0.845816C3.76436 -0.270037 1.95247 -0.270037 0.836773 0.845816C-0.278924 1.96167 -0.278924 3.77382 0.836773 4.88967L17.9739 22.0292L17.9828 22.0203Z"
+            fill="#12471E"
+          />
+        </svg>
+      </button>
+      <div className={clsx(isShowing ? "block" : "hidden")}>{children}</div>
+    </div>
+  );
+}
