cf2174be403606e84642f397e8b1344cae67f449
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-23 09:25:31 +0200

    Updates

--- app/(site)/components/BorderLines.jsx
+++ app/(site)/components/BorderLines.jsx
@@ -4,9 +4,22 @@ export default function BorderLines({ children, className, side }) {
       return (
         <div className={className}>
           <div className="p-4 pt-8 relative">
-            <div className="absolute left-0 right-0 top-4 h-[1px] bg-black" />
-            <div className="absolute right-0 bottom-0 top-0 w-[1px] bg-black" />
-            <div className="absolute top-[14px] right-[-2px] h-[5px] w-[5px] bg-black" />
+            <div className="absolute left-0 right-0 top-4 h-[1px] bg-darkGreen" />
+            <div className="absolute right-0 bottom-0 top-0 w-[1px] bg-darkGreen" />
+            <div className="absolute top-[14px] right-[-2px] h-[5px] w-[5px] bg-darkGreen" />
+            {children}
+          </div>
+        </div>
+      );
+    case "center":
+      return (
+        <div className={className}>
+          <div className="p-4 pt-8 relative">
+            <div className="absolute left-0 right-0 top-4 h-[1px] bg-darkGreen" />
+            <div className="absolute right-0 bottom-0 top-0 w-[1px] bg-darkGreen" />
+            <div className="absolute top-[14px] right-[-2px] h-[5px] w-[5px] bg-darkGreen" />
+            <div className="absolute left-0 bottom-0 top-0 w-[1px] bg-darkGreen" />
+            <div className="absolute top-[14px] left-[-2px] h-[5px] w-[5px] bg-darkGreen" />
             {children}
           </div>
         </div>
@@ -15,9 +28,9 @@ export default function BorderLines({ children, className, side }) {
       return (
         <div className={className}>
           <div className="p-4 pt-8 relative">
-            <div className="absolute left-0 right-0 top-4 h-[1px] bg-black" />
-            <div className="absolute left-0 bottom-0 top-0 w-[1px] bg-black" />
-            <div className="absolute top-[14px] left-[-2px] h-[5px] w-[5px] bg-black" />
+            <div className="absolute left-0 right-0 top-4 h-[1px] bg-darkGreen" />
+            <div className="absolute left-0 bottom-0 top-0 w-[1px] bg-darkGreen" />
+            <div className="absolute top-[14px] left-[-2px] h-[5px] w-[5px] bg-darkGreen" />
             {children}
           </div>
         </div>
--- app/(site)/page.jsx
+++ app/(site)/page.jsx
@@ -1,11 +1,17 @@
 import { getHome } from "../../sanity/fragments/sanity-utils";
 import RenderModules from "./components/RenderModules";
+import BorderLines from "./components/BorderLines";
 
 export default async function Home() {
   const home = await getHome();
   return (
     <main>
       <RenderModules modules={home?.modules} />
+      <div className="flex">
+        <BorderLines side="right">Hej med dig</BorderLines>
+        <BorderLines side="center">Hej med dig</BorderLines>
+        <BorderLines side="left">Hej med dig</BorderLines>
+      </div>
     </main>
   );
 }
