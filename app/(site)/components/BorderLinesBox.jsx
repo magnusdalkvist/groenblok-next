a75f42503920fead2af3661bb8301be1c2da11c6
Magnus Dalkvist <magn552n@stud.kea.dk>
2023-05-31 19:08:30 +0200

    footer mobile responsive

--- app/(site)/components/BorderLinesBox.jsx
+++ app/(site)/components/BorderLinesBox.jsx
@@ -1,15 +1,15 @@
+import clsx from "clsx";
+
 export default function BorderLinesBox({ children, className }) {
   return (
-    <div className={className}>
-      <div className=" relative">
-        <div className="absolute left-0 right-[30%] top-4 h-[1px] bg-darkGreen" />
-        <div className="absolute left-0 bottom-[30%] top-0 w-[1px] bg-darkGreen" />
-        <div className="absolute top-[13px] left-[-3px] h-[7px] w-[7px] bg-darkGreen" />
-        {children}
-        <div className="absolute left-[30%] right-0 bottom-4 h-[1px] bg-darkGreen" />
-        <div className="absolute right-0 bottom-0 top-[30%] w-[1px] bg-darkGreen" />
-        <div className="absolute bottom-[13px] right-[-3px] h-[7px] w-[7px] bg-darkGreen" />
-      </div>
+    <div className="relative p-4">
+      <div className="absolute left-0 right-[30%] top-4 h-[1px] bg-darkGreen" />
+      <div className="absolute left-4 bottom-[30%] top-0 w-[1px] bg-darkGreen" />
+      <div className="absolute left-4 -translate-x-[3px] -translate-y-[3px] h-[7px] w-[7px] bg-darkGreen" />
+      <div className="absolute left-[30%] right-0 bottom-4 h-[1px] bg-darkGreen" />
+      <div className="absolute right-4 bottom-0 top-[30%] w-[1px] bg-darkGreen" />
+      <div className="absolute right-4 bottom-4 translate-x-[3px] translate-y-[3px] h-[7px] w-[7px] bg-darkGreen" />
+      <div className={className}>{children}</div>
     </div>
   );
 }
